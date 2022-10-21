ec4f57cb40b787d2114cef174b66966311887fa3
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2022-10-21 16:04:03 +0200

    Up

--- api_examples/README.md
+++ api_examples/README.md
@@ -3,6 +3,13 @@
 You can find your API key in your profile page.
 
 ```bash
-export MAGNETDB_API_KEY=
+export MAGNETDB_API_KEY=b61a58bb8edd4d8bbc03d8bc4d88be8a
 poetry run python -m api_examples.list_magnet
 ```
+Do not forget to install python_magnetdb
+
+```bash
+cd python_magnetdb
+poetry install
+```
+
--- api_examples/list_magnet.py
+++ api_examples/list_magnet.py
@@ -2,6 +2,6 @@ import os
 
 import requests
 
-r = requests.get("http://localhost:8000/api/magnets", headers={ 'Authorization': os.getenv('MAGNETDB_API_KEY') })
+r = requests.get("http://localhost:8000/api/magnets", headers={'Authorization': os.getenv('MAGNETDB_API_KEY')})
 for magnet in r.json()['items']:
     print(f"MAGNET: {magnet['name']} ({magnet['status']})")
