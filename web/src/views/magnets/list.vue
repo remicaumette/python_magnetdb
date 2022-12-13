9fb4f234bc7e8af0d68ccb5206c6370db278cbb6
Remi Caumette <remicaumette@icloud.com>
2022-12-13 21:59:49 +0100

    Add commissioned_at and decommissioned_at to web

--- web/src/views/magnets/list.vue
+++ web/src/views/magnets/list.vue
@@ -34,6 +34,12 @@
         <template v-slot:item.updated_at="{ item }">
           {{ item.updated_at | datetime }}
         </template>
+        <template v-slot:item.commissioned_at="{ item }">
+          {{ item.commissioned_at | datetime }}
+        </template>
+        <template v-slot:item.decommissioned_at="{ item }">
+          {{ item.decommissioned_at | datetime }}
+        </template>
       </DataTable>
     </Card>
   </div>
@@ -83,6 +89,16 @@ export default {
         {
           key: 'updated_at',
           name: 'Updated At',
+          sortable: true,
+        },
+        {
+          key: 'commissioned_at',
+          name: 'Commissioned At',
+          sortable: true,
+        },
+        {
+          key: 'decommissioned_at',
+          name: 'Decommissioned At',
           sortable: true,
         },
       ]
