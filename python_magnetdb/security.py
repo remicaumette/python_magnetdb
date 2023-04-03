9d9fbc7c5bc0110d64bc881329baf7629dd781e7
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2023-04-03 17:09:06 +0200

    up with lncmi network change

--- python_magnetdb/security.py
+++ python_magnetdb/security.py
@@ -3,7 +3,7 @@ import jwt
 from datetime import datetime
 from os import getenv
 
-authorization_server = getenv('SECURITY_AUTHORIZATION_SERVER') or "http://sso.grenoble.lncmi.local"
+authorization_server = getenv('SECURITY_AUTHORIZATION_SERVER') or "http://sso.lncmig.local"
 front_authorization_server = getenv('FRONT_SECURITY_AUTHORIZATION_SERVER') or authorization_server
 authorization_host_server = getenv('FRONT_SECURITY_AUTHORIZATION_HOST_SERVER')
 client_id = getenv('SECURITY_CLIENT_ID') or "testid"
